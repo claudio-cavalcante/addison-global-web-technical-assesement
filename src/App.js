88a82ead0c8d2e5840f29477e169693857a5e302
Claudio Cavalcante Tonha <claudio.cth@gmail.com>
2021-01-26 23:26:46 -0300

    Applied behaviors of buttons

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+
+ 
+  const [activeId, setActiveId] = React.useState(1);
+
+  const buttons = ["All promotions", "New customers"];
+
+  const handleClick = index => {
+    setActiveId(index);
+  }
+
+  return (
+    <div className="App">
+      <div className="row">
+        <div className="col-6">
+          {buttons.map((el, index) => {
+            var className = `btn ${activeId === index ? "active" : ""}`;
+
+            return (
+              <button onClick={() => handleClick(index)} key={index} className={className}>{el}</button>
+            )
+
+          })}
+        </div>
+      </div>
+      <div className="promotions">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
